e5246a44b494be01062a528057fb483ab0098f0a
blissb <blissb4096@protonmail.com>
2025-03-25 02:13:59 -0700

    fix main

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@
 mod application;
 
 fn main() {
-    let mut app = application::Application::new(600, 400);
-    println!("Application initialized");
-    app.run();
+    {
+        let mut app = application::Application::new(600, 400);
+        println!("Application initialized");
+        app.run();
+        println!("Exiting...");
+    }
     println!("Application destroyed");
 }
 
