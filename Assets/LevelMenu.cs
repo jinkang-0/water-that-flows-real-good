d5edef44766498a9733c715e44f09c4fb57d6ff6
AnnPBasil <137114919+AnnPBasil@users.noreply.github.com>
2025-05-02 12:25:51 -0700

    Add main menu and level select (#13)
    * finished menu and level ui
    
    * Saving some changes
    
    * updated drain cells
    
    * Updated UI Assets
    
    * Added more assets
    
    * Fixed saving

---
+++ Assets/LevelMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour
+{
+    public void OpenLevel(int levelId) {
+        SceneManager.LoadScene(levelId);
+    }
+}
