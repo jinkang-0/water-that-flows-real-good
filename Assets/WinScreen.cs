0f190f5c50c03b63aca1ad46437c2a24d335a4d6
AnnPBasil <ann.basil@outlook.com>
2025-05-02 14:30:06 -0700

    Working on win screen

---
+++ Assets/WinScreen.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+    public void ReturnToMenu() {
+        SceneManager.LoadSceneAsync(0);
+    }
+}
