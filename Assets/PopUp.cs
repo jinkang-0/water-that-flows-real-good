a1b795d27a480a963615d78167badff7e532d0cc
AnnPBasil <ann.basil@outlook.com>
2025-05-03 17:17:25 -0700

    Made the scores specific per level

--- Assets/PopUp.cs
+++ Assets/PopUp.cs
@@ -8,9 +8,25 @@ public class PopUp : MonoBehaviour
 {
     public GameObject popupObject;
     public Simulation sim;
+    public int levelnum;
+
+    
 
     public void Update() {
-        if (sim.score >= sim.numParticles * 1/4000 && !popupObject.activeSelf)
+        Dictionary<int, int> levelScores = new Dictionary<int, int>
+        {
+            { 1, sim.numParticles * 3/4 },  
+            { 2, sim.numParticles * 1/10 },  
+            { 3, sim.numParticles * 3/4 },
+            { 4, sim.numParticles * 3/4 },
+            { 5, sim.numParticles * 3/4 },   
+            { 6, sim.numParticles * 3/4 },
+            { 7, sim.numParticles * 1/10 },
+            { 8, sim.numParticles * 1/50 }, 
+            { 9, sim.numParticles * 3/4 } 
+        };
+
+        if (sim.score >= levelScores[levelnum] && !popupObject.activeSelf)
         {
             popupObject.SetActive(true);
         }
